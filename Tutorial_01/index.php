ea112380592ec30824684f5072a02639b13c4cbe
Mr-Khant <103758712+Mr-Khant@users.noreply.github.com>
2023-03-28 22:33:16 +0630

    Update

--- Tutorial_01/index.php
+++ Tutorial_01/index.php
@@ -2,7 +2,39 @@
 
 function drawChessBorad($rows, $cols)
 {
-    // your code here
+    if ($rows <= 0 || $cols <= 0) {
+        if ($rows <= 0 && $cols <= 0) {
+            echo '<p class="error-message">$rows and $cols must be greater than zero</p>';
+        } elseif ($rows <= 0) {
+            echo '<p class="error-message">$rows must be greater than zero</p>';
+        } elseif ($cols <= 0) {
+            echo '<p class="error-message">$cols must be greater than zero</p>';
+        }
+    } elseif (!is_numeric($rows) || !is_numeric($cols)) {
+        if (!is_numeric($rows) && !is_numeric($cols)) {
+            echo '<p class="error-message">$rows and $cols must be numbers</p>';
+        } elseif (!is_numeric($rows)) {
+            echo '<p class="error-message">$rows must be a number</p>';
+        } elseif (!is_numeric($cols)) {
+            echo '<p class="error-message">$cols must be a number</p>';
+        } elseif ($rows <= 0 && !is_numeric($cols)) {
+            echo '<p class="error-message">$rows parameter must be greater than 0. $cols parameter must be a number.</p>';
+        } elseif (!is_numeric($rows) && $cols <= 0) {
+            echo '<p class="error-message">$rows parameter must be a number. $cols parameter must be greater than 0</p>';
+        }
+    }else {
+        for ($i = 1; $i <= $rows; $i++) {
+            echo '<tr class="row">';
+            for ($j = 1; $j <= $cols; $j++) {
+                if (($i + $j) % 2 == 0) {
+                    echo '<td class="col color1"></td>';
+                } else {
+                    echo '<td class="col color2"></td>';
+                }
+            }
+            echo '</tr>';
+        }
+    }
 }
 
 // drawChessBorad(8, 8); // output => 8 rows and 8 columns chess borad.
@@ -15,3 +47,26 @@ function drawChessBorad($rows, $cols)
 // drawChessBorad(5, 'mycols'); // output => $cols parameter must be number.
 // drawChessBorad(0, 'mycols'); // output => $rows parameter must be greater than 0.$cols parameter must be number.
 // drawChessBorad('myrow', 0); // output => $rows parameter must be number.$cols parameter must be greater than 0.
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>ChessBoard</title>
+</head>
+<body>
+    <div class="mid-container">
+        <h1>Chessboard</h1>
+        <table class="chess">
+            <?php drawChessBorad(8 , 8); ?>
+        <table>
+    </div>
+</body>
+</html>
+
--- Tutorial_02/index.php
+++ Tutorial_02/index.php
@@ -2,7 +2,28 @@
 
 function makeDiamondShape($row)
 {
-    # your code here
+    if ($row <= 0 ) {
+        echo '<p class="error-message">$row must be greater than zero</p>';
+    } elseif (!is_numeric($row)) {
+        echo '<p class="error-message">$rows must be a number</p>';
+    } elseif ($row % 2 == 0) {
+        echo '<p class="error-message">$row must be odd number.</p>';
+    }else {
+        for ($i = 0; $i < $row; $i++) {
+            $numSpaces = abs($row - 2 * $i - 1) / 2;
+            $numStars = $row - $numSpaces * 2;
+    
+            for ($j = 0; $j < $numSpaces; $j++) {
+                echo "&nbsp ";
+            }
+    
+            for ($j = 0; $j < $numStars; $j++) {
+                echo "*";
+            }
+    
+            echo "<br/>";
+        }
+    }
 }
 
 // Result
@@ -40,3 +61,24 @@ function makeDiamondShape($row)
 //makeDiamondShape('three');
 // output
 // $row parameter must be number.
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Diamond</title>
+</head>
+<body>
+    <h1>Diamond</h1>
+    <div class="mid-container">
+        <div class="diamond">
+            <?php echo makeDiamondShape(7); ?>
+        </div>
+    </div>
+</body>
+</html>
