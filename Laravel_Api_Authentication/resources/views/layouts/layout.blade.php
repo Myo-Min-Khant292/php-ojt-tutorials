3d8d98d8a8c50e1d7a0e6b432233ace974d46b22
Mr-Khant <103758712+Myo-Min-Khant292@users.noreply.github.com>
2023-05-05 09:31:09 +0630

    update

--- Laravel_Api_Authentication/resources/views/layouts/layout.blade.php
+++ Laravel_Api_Authentication/resources/views/layouts/layout.blade.php
@@ -17,7 +17,7 @@
     <body class="antialiased">
         <div class="header">
             <a href="#" class="auth auth1">Test</a>
-            <a href="#" class="auth auth2">Login</a> 
+            <a href="#" class="auth auth2">Test</a> 
             <div class="header-nav clearfix">
                 <h2>NavBar</h2>
                 <a href="{{route('major#index')}}">Majors</a>
